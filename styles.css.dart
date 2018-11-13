8db22d749ee49fdfb032a9818ac2d6128fd365c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-13 18:33:11 +0000

    Deploy up-trend/houston-web to github.com/up-trend/houston-web.git:gh-pages

---
+++ styles.css.dart
@@ -0,0 +1,4 @@
+library asset_houston_web_web_styles.css.dart;
+
+
+final List<dynamic> styles = ['@import url(https://fonts.googleapis.com/css?family=Roboto);\n@import url(https://fonts.googleapis.com/css?family=Material+Icons);\n\nbody {\n  max-width: 600px;\n  margin: 0 auto;\n  padding: 5vw;\n}\n\n* {\n  font-family: Roboto, Helvetica, Arial, sans-serif;\n}\n'];
---
+++ styles.css.shim.dart
@@ -0,0 +1,4 @@
+library asset_houston_web_web_styles.css.shim.dart;
+
+
+final List<dynamic> styles = ['@import url(https://fonts.googleapis.com/css?family=Roboto); @import url(https://fonts.googleapis.com/css?family=Material+Icons);body._ngcontent-%ID%{max-width:600px;margin:0 auto;padding:5vw}*._ngcontent-%ID%{font-family:Roboto,Helvetica,Arial,sans-serif}'];
